9b6015a4fd6f0f4d47d0325bf829df703728a0a5
mohamedaix2 <mohamed.ouchnak@icloud.com>
2026-09-13 15:45:42 +0200

    Bump Android Brumes to 0.5.0

--- AndroidBrumes/app/build.gradle.kts
+++ AndroidBrumes/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mumu.brumes"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
